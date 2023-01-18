b372c44c66aa402cef6c7094a7636b00c8034540
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-18 12:34:42 +0530

    Refactor1.1 added

--- MoodAnalyzer.cs
+++ MoodAnalyzer.cs
@@ -6,12 +6,43 @@ using System.Threading.Tasks;
 
 namespace MoodAnalyzer
 {
-    public class MoodAnalyzer
+    public class MoodAnalyzer1
     {
-        public static string analyseMood(string message)
+        public string analyseMood(string message)
         {
+            if (message == "I am in Sad Mood")
+            {
+                message = "SAD";
+                return message;
 
-            return message;
+            }
+            else
+            {
+                message = "HAPPY";
+                return message;
+            }
+        }
+
+    }
+    public class MoodAnalyser1
+    {
+
+        public string msg;
+        public MoodAnalyser1()
+        {
+            this.msg = "SAD";
+        }
+        public MoodAnalyser1(string msg1)
+        {
+            this.msg = msg1;
+
+        }
+
+        public string analyseMood1()
+        {
+            MoodAnalyser1 obja = new MoodAnalyser1();
+            return obja.msg;
+
         }
     }
 }
