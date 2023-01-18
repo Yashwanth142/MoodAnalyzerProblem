2f28a707b1a61d3c1eccef234c81f195b71e75aa
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-18 12:19:16 +0530

    Message

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        public static string analyseMood(string message)
+        {
+
+            return message;
+        }
+    }
+}
