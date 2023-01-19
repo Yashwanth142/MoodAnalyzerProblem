341133a7c9dd7e56f66cb13d103665e3bfe86851
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-19 17:50:04 +0530

    refactor code 2.1

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
             MoodAnalyzer1 moodAnalyzer1 = new MoodAnalyzer1();
             moodAnalyzer1.analyseMood(message);
 
-            MoodAnalyser1 moodAnalyser = new MoodAnalyser1();
+            MoodAnalyser1  moodAnalyser = new MoodAnalyser1();
             string r2 = moodAnalyser.analyseMood1();
 
             string r1 = moodAnalyser.msg;
