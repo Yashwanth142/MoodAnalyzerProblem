a58e8b1a60701e52297c9541ba6053ea8fd546f0
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-19 17:41:21 +0530

    Invalid Mood added

---
+++ MoodAnalyzerNullException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzerNullException : Exception
+    {
+        public MoodAnalyzerNullException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
