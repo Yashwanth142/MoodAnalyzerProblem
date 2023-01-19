9853bf1ea501b955c2c81cb9a0cb912456d2ae76
Yashwanth142 <119283440+Yashwanth142@users.noreply.github.com>
2023-01-19 18:20:05 +0530

    UniteTest added

---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+using ModeAnalyzerAssignment;
+using System.Diagnostics.SymbolStore;
+
+namespace TestCaseMoodAnalyzer
+{
+   
+ [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void UseCase4_1()
+        {
+            object expected = new MoodAnalyzer1();
+            object returned = Mood_Aanalyzer_Factory.CreateMoodAnalyze("ModeAnalyzerAssignment.MoodAnalyzer", "MoodAnalyzer");
+            expected.Equals(returned);
+        }
+      
+        
+    }
+}
