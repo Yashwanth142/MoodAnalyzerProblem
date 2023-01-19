23c2cc15e6aa8715b7638f2ee61cbbaadeb42218
Yashwanth142 <119283440+Yashwanth142@users.noreply.github.com>
2023-01-19 18:03:23 +0530

    Testcase3.1 added

---
+++ Testcase.cs
@@ -0,0 +1,55 @@
+using ModeAnalyzerAssignment;
+using System.Diagnostics.SymbolStore;
+
+namespace TestCaseMoodAnalyzer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCase1_1()
+        {
+            string input = "I am in Sad Mood";
+            string expected = "SAD";
+
+
+            MoodAnalyzer1 moodAnalyzer = new MoodAnalyzer1();
+
+            string actual = moodAnalyzer.analyseMood(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestCase1_2()
+        {
+            string input = "I am in Any Mood";
+            string expected = "HAPPY";
+
+
+            MoodAnalyzer1 moodAnalyzer = new MoodAnalyzer1();
+
+            string actual = moodAnalyzer.analyseMood(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+       
+
+       
+        [TestMethod]
+        public void UseCase3_1()
+        {
+            try
+            {
+               string input = null;
+               var analyze = new MoodAnalyser1(input);
+            }
+            catch (NullReferenceException Exception)
+            {
+                Assert.AreEqual("Mood can not be Null.", Exception.Message);
+            }
+        }
+
+    }
+}
