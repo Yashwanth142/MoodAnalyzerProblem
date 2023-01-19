763ed157da8f701d205ad73a46bce7a6be55f82b
Yashwanth142 <119283440+Yashwanth142@users.noreply.github.com>
2023-01-19 18:23:38 +0530

    testcase 4.2 added

---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+using ModeAnalyzerAssignment;
+using System.Diagnostics.SymbolStore;
+
+namespace TestCaseMoodAnalyzer
+{
+   
+ [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void UseCase4_1()
+        {
+            object expected = new MoodAnalyzer1();
+            object returned = Mood_Aanalyzer_Factory.CreateMoodAnalyze("ModeAnalyzerAssignment.MoodAnalyzer", "MoodAnalyzer");
+            expected.Equals(returned);
+        }
+      
+          [TestMethod]
+        public void UseCase4_2()
+        {
+            try
+            {
+                object returned = Mood_Aanalyzer_Factory.CreateMoodAnalyze("ModeAnalyzerAssignment.Mood", "Mood");
+            }
+            catch (MoodAnalyzerNullException Exception)
+            {
+                Assert.AreEqual("Class not found", Exception.Message);
+            }
+        }
+    }
+}
